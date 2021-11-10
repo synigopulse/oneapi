4d0091699b8407369ec957afb31327ffcb6421d0
Jasmin Memišević <jasmin.memisevic@isatis.com>
2021-11-10 21:07:51 +0100

    models rearanged in folder , added one missing model ComponentOfferingAssociation

---
+++ Synigo.OpenEducationApi/Model/V4/Models/ComponentOfferingAssociation.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Synigo.OpenEducationApi.Model.V4.Models
+{
+    public class ComponentOfferingAssociation<TResult> : Association<TResult> where TResult : ComponentResult
+    {
+        [JsonPropertyName("result")]
+        public override TResult Result { get; set; }
+    }
+}
