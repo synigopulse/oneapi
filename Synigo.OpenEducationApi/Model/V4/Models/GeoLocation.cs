55713c01d75c596e2089c15f6176cba2d6ac941c
Jasmin Memišević <jasmin.memisevic@isatis.com>
2021-11-12 07:13:04 +0100

    added guid to geolocation

--- Synigo.OpenEducationApi/Model/V4/Models/GeoLocation.cs
+++ Synigo.OpenEducationApi/Model/V4/Models/GeoLocation.cs
@@ -1,12 +1,20 @@
-﻿using System.Text.Json.Serialization;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace Synigo.OpenEducationApi.Model.V4
 {
     public class Geolocation
     {
+        [Required]
+        [JsonPropertyName("geoLocationId")]
+        public Guid GeoLocationId { get; set; }
+
+        [Required]
         [JsonPropertyName("latitude")]
         public double Latitude { get; set; }
 
+        [Required]
         [JsonPropertyName("longitude")]
         public double Longitude { get; set; }
     }
