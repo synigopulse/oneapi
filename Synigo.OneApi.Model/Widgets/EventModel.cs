8090484068595d6e00fb8fdfa51e5674723f4967
Amela Krezic <a.krezic@synigo.nl>
2021-11-17 15:39:05 +0100

    added EventModel

---
+++ Synigo.OneApi.Model/Widgets/EventModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Graph;
+
+namespace Synigo.OneApi.Model.Widgets
+{
+    public class EventModel:Event
+    {
+        public static readonly string SynigoType = "https://synigo.model#event";
+    }
+}
