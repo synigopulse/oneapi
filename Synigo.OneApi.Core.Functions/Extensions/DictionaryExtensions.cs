bef7be06ef6a8b01717bb543284611016f72855b
Jasmin Memišević <jasmin.memisevic@isatis.com>
2021-12-08 07:58:10 +0100

    extensions added for function use

--- Synigo.OneApi.Core.Functions/Extensions/DictionaryExtensions.cs
+++ Synigo.OneApi.Core.Functions/Extensions/DictionaryExtensions.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Extensions.Primitives;
 using Synigo.OneApi.Common.Extensions;
+using Synigo.OneApi.Model.Requests;
 
 namespace Synigo.OneApi.Core.Functions.Extensions
 {
@@ -26,5 +27,48 @@ namespace Synigo.OneApi.Core.Functions.Extensions
                 ? dict[key].First().GetEnumValueNullable<T>() : null;
         }
 
+        public static Nullable<T> GetDataOrNull<T>(this Dictionary<string, StringValues> dict, string key)
+           where T : struct
+        {
+            if (!dict.ContainsKey(key) || string.IsNullOrEmpty(dict[key].FirstOrDefault()))
+                return null;
+
+            return (T)Convert.ChangeType(dict[key].FirstOrDefault(), typeof(T));
+        }
+
+        public static List<T> GetEnumListValues<T>(this Dictionary<string, StringValues> dict, string key)
+         where T : Enum
+        {
+            return dict.ContainsKey(key) && dict[key].Count() > 0 ?
+                dict[key].Select(s => s.GetEnumValue<T>()).ToList() : new List<T>();
+        }
+
+        public static List<string> GetStringListValues(this Dictionary<string, StringValues> dict, string key)
+        {
+            return dict.ContainsKey(key) ?
+                dict[key].ToList() : new List<string>();
+        }
+
+        public static Guid? GetGuidValueOrNull(this Dictionary<string, StringValues> dict, string key)
+        {
+            if (!dict.ContainsKey(key) || string.IsNullOrEmpty(dict[key].FirstOrDefault()))
+                return null;
+
+            if (!Guid.TryParse(dict[key].FirstOrDefault(), out var result))
+                return null;
+
+            return result;
+        }
+
+        public static PageRequest<T> GetPageRequest<T>(this Dictionary<string, StringValues> dict)
+          where T : class, new()
+        {
+            var request = new PageRequest<T>() { Request = new T() };
+            request.PageNumber = dict.ContainsKey("pageNumber") ? Convert.ToInt32(dict["pageNumber"].First()) : 1;
+            request.PageSize = dict.ContainsKey("pageSize") ? Convert.ToInt32(dict["pageSize"].First()) : 10;
+            request.Sort = dict.ContainsKey("sort") ? dict["sort"].ToList() : new List<string>();
+
+            return request;
+        }
     }
 }
