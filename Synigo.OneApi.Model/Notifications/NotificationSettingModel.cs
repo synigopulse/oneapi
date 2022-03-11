e8ae909ec50934143ebc78c9f2a863dfd4c0d2a3
Amela Krezic <a.krezic@synigo.nl>
2022-03-11 19:29:37 +0100

    added notification settings model

---
+++ Synigo.OneApi.Model/Notifications/NotificationSettingModel.cs
@@ -0,0 +1,33 @@
+﻿using Synigo.OneApi.Model.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Synigo.OneApi.Model.Notifications
+{
+    public class NotificationSettingModel
+    {
+        public static readonly string SynigoType = "https://synigo.model#notificationSettings";
+
+        /// <summary>
+        /// Get or set multilanguage title
+        /// </summary>
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Get or set notification type
+        /// </summary>
+        [JsonPropertyName("notificationType")]
+        public string NotificationType { get; set; }
+
+        /// <summary>
+        /// Get or set notification active status
+        /// </summary>
+        [JsonPropertyName("isActive")]
+        public bool IsActive { get; set; }
+    }
+}
