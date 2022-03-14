b4505040778938fb738eea89262bd679b50c6178
amelakrezic <87751853+amelakrezic@users.noreply.github.com>
2022-03-14 10:46:09 +0100

    Added notification settings model (#12)
    * added notification settings model

---
+++ Synigo.OneApi.Model/Notifications/NotificationSettingModel.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Synigo.OneApi.Model.Notifications
+{
+    public class NotificationSettingModel
+    {
+        public static readonly string SynigoType = "https://synigo.model#notificationSettings";
+
+        /// <summary>
+        /// Get or set title
+        /// </summary>
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Get or set notification type
+        /// </summary>
+        [JsonPropertyName("notificationType")]
+        public string NotificationType { get; set; }
+
+        /// <summary>
+        /// Get or set notification active status
+        /// </summary>
+        [JsonPropertyName("isActive")]
+        public bool IsActive { get; set; }
+    }
+}
