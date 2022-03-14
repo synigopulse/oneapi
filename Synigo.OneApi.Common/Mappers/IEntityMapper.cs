947b3d885fe8143ad7c0b07ec9a90c1a1ceff630
Jasmin Memišević <jasmin.memisevic@isatis.com>
2022-03-14 09:35:56 +0100

    adding entity mapper to one api package

---
+++ Synigo.OneApi.Common/Mappers/IEntityMapper.cs
@@ -0,0 +1,8 @@
+﻿namespace Synigo.OneApi.Common.Mappers
+{
+    public interface IEntityMapper<TIn, TOut>
+       where TOut : class where TIn : class
+    {
+        TOut Map(TIn source, TOut dest = null);
+    }
+}
---
+++ Synigo.OneApi.Common/Mappers/IEntityMapperAsync.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace Synigo.OneApi.Common.Mappers
+{
+    public interface IEntityMapperAsync<TIn, TOut>
+          where TOut : class where TIn : class
+    {
+        Task<TOut> MapAsync(TIn source, TOut dest = null);
+    }
+}
