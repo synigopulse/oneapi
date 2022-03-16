58dacc43b2f3f885adfca95628fc2d2b967a6e52
Amela Krezic <a.krezic@synigo.nl>
2022-03-16 15:29:07 +0100

    added SynigoApiClient and fixed NotificationsClient

--- Synigo.OneApi.Clients/Notifications/NotificationsClient.cs
+++ Synigo.OneApi.Clients/Notifications/NotificationsClient.cs
@@ -1,51 +1,36 @@
 ﻿using Microsoft.Extensions.Configuration;
-using Synigo.OneApi.Clients.Notifications.Models;
-using System;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Text.Json;
-using System.Threading.Tasks;
-using System.Linq;
-using Microsoft.IdentityModel.Clients.ActiveDirectory;
 using Newtonsoft.Json;
+using Synigo.OneApi.Clients.Notifications.Models;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace Synigo.OneApi.Clients.Notifications
 {
     public class NotificationsClient : INotificationsClient
     {
-        private string _synigoUrl = "https://synigo.azure-api.net/test/";
         private readonly IConfiguration Configuration;
+        private readonly string _tenantId;
+        private readonly string _synigoApiUrl;
 
         public NotificationsClient(IConfiguration configuration)
         {
             Configuration = configuration;
+            _tenantId = Configuration.GetSection("AzureAd").GetValue<string>("TenantId");
+            _synigoApiUrl = Configuration.GetSection("AzureAd").GetValue<string>("SynigoApiUrl");
         }
 
-        public async Task SendNotification(NotificationSource notification)
+        /// <summary>
+        /// Sends portal notification to all users in organization
+        /// </summary>
+        /// <param name="notification" cref="NotificationSource">received NotificationSource model</param>
+        /// <returns cref="HttpResponseMessage">Returns HttpResponseMessage</returns>
+        public async Task<HttpResponseMessage> SendNotification(NotificationSource notification)
         {
-            var clientId = Configuration.GetSection("AzureAd").GetValue<string>("ClientId");
-            var clientSecret = Configuration.GetSection("AzureAd").GetValue<string>("ClientSecret");
-            var tenantId = Configuration.GetSection("AzureAd").GetValue<string>("TenantId");
-            var messageNotification = new MessageNotification(tenantId, notification);
-            using(var client = new HttpClient())
-            {
-                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, $"{_synigoUrl}/messagenotifications/{tenantId}/messageNotification");
-                request.Headers.Add("x-clientid", clientId);
-                request.Headers.Add("x-clientsecret", clientSecret);
-                request.Headers.Add("x-tenantId", tenantId);
-                var token = await GetToken(tenantId, clientId, clientSecret);
-                request.Headers.Add("x-token", token);
-                request.Content = new StringContent(JsonConvert.SerializeObject(messageNotification));
-                await client.SendAsync(request);
-            }
-        }
-
-        public async Task<string> GetToken(string tenantId, string clientId, string clientSecret)
-        {
-            var clientCredential = new ClientCredential(clientId, clientSecret);
-            var context = new AuthenticationContext($"https://login.microsoftonline.com/{tenantId}");
-            var authenticationResult = await context.AcquireTokenAsync("https://graph.microsoft.com", clientCredential);
-            return authenticationResult?.AccessToken;
+            var client = new SynigoApiClient(Configuration);
+            var messageNotification = new MessageNotification(_tenantId, notification);
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, $"{_synigoApiUrl}/messagenotifications/{_tenantId}/messageNotification");
+            request.Content = new StringContent(JsonConvert.SerializeObject(messageNotification));
+            return await client.SendAsync(request);
         }
     }
 }
---
+++ Synigo.OneApi.Clients/SynigoApiClient.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Synigo.OneApi.Clients
+{
+    public class SynigoApiClient
+    {
+        protected readonly IConfiguration Configuration;
+        protected readonly string _clientId;
+        protected readonly string _clientSecret;
+        protected readonly string _tenantId;
+        protected readonly string _synigoApiUrl;
+
+        public SynigoApiClient(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            _clientId = Configuration.GetSection("AzureAd").GetValue<string>("ClientId");
+            _clientSecret = Configuration.GetSection("AzureAd").GetValue<string>("ClientSecret");
+            _tenantId = Configuration.GetSection("AzureAd").GetValue<string>("TenantId");
+            _synigoApiUrl = Configuration.GetSection("AzureAd").GetValue<string>("SynigoApiUrl");
+        }
+
+        public async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                request = await AuthorizeRequest(request);
+                return await httpClient.SendAsync(request);
+            }
+        }
+
+        private async Task<HttpRequestMessage> AuthorizeRequest(HttpRequestMessage request)
+        {
+            request.Headers.Add("x-clientid", _clientId);
+            request.Headers.Add("x-clientsecret", _clientSecret);
+            request.Headers.Add("x-tenantId", _tenantId);
+            var token = await AcquireTokenAsync();
+            request.Headers.Add("x-token", token);
+            return request;
+        }
+
+        private async Task<string> AcquireTokenAsync()
+        {
+            var clientCredential = new ClientCredential(_clientId, _clientSecret);
+            var context = new AuthenticationContext($"https://login.microsoftonline.com/{_tenantId}");
+            var authenticationResult = await context.AcquireTokenAsync("https://graph.microsoft.com", clientCredential);
+            return authenticationResult?.AccessToken;
+        }
+    }
+}
