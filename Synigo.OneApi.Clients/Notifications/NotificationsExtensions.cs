657ba9596f435feee00d5fcf70817766f207e39a
amelakrezic <87751853+amelakrezic@users.noreply.github.com>
2022-04-01 10:32:22 +0200

    added package info for Synigo.OneApi.Clients, added NotificationsExtensions and removed Clients reference from Core project (#14)
    * added package info for Synigo.OneApi.Clients
    
    * added NotificationsExtensions and removed Clients reference from Core project

---
+++ Synigo.OneApi.Clients/Notifications/NotificationsExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Synigo.OneApi.Core.WebApi;
+
+namespace Synigo.OneApi.Clients.Notifications
+{
+    public static class NotificationsExtensions
+    {
+        public static OneApiBuilder UseNotificationsClient(this OneApiBuilder builder)
+        {
+            builder.Services.AddScoped<INotificationsClient, NotificationsClient>();
+            return builder;
+        }
+    }
+}
