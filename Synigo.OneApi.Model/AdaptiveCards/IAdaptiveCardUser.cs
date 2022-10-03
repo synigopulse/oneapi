bad929ab1d8c4a99edd0a953fadbc3a74f01d0c0
Volkan Baykal <v.baykal@synigo.nl>
2022-10-03 11:14:06 +0200

    * Added new adaptivecard models including user and response. * Added new grah provider interface and abstract implementation. * Added new extension method to add a new graph provider.

---
+++ Synigo.OneApi.Model/AdaptiveCards/IAdaptiveCardUser.cs
@@ -0,0 +1,14 @@
+﻿namespace Synigo.OneApi.Model.AdaptiveCards
+{
+    /// <summary>
+    /// The base implementation required for an adaptive card user.
+    /// Classes should implement this and add additional fields themselves.
+    /// </summary>
+    public interface IAdaptiveCardUser
+    {
+        /// <summary>
+        /// The username of the user.
+        /// </summary>
+        string Username { get; set; }
+    }
+}
