d7bba784d9ba1c55ead6e5078df7ab7a55ad44bf
Volkan Baykal <106151676+volkanbaykal@users.noreply.github.com>
2022-10-14 15:28:47 +0200

    HOTFIX: fixed an issue with polymorphic JSON deserialization (#18)
    * Added new adaptive-card models including user and response.
    * Added new graph provider interface and abstract implementation.
    * Added new extension method to add a new graph provider.
    * Cleaned up using statements and updated comments.
    
    * HOTFIX: fixed an issue with polymorphic deserialization

--- Synigo.OneApi.Model/Response/AdaptiveCardResponse.cs
+++ Synigo.OneApi.Model/Response/AdaptiveCardResponse.cs
@@ -1,4 +1,4 @@
-﻿using Synigo.OneApi.Model.AdaptiveCards;
+using Synigo.OneApi.Model.AdaptiveCards;
 using System.Text.Json.Serialization;
 
 namespace Synigo.OneApi.Model.Response
@@ -7,18 +7,18 @@ namespace Synigo.OneApi.Model.Response
     /// A response template used for adaptive cards.
     /// </summary>
     /// <typeparam name="T">A class definition containing additional values used in the adaptive card.</typeparam>
-    public class AdaptiveCardResponse<T>
+    public class AdaptiveCardResponse<TUser, TValue> where TUser : IAdaptiveCardUser
     {
         /// <summary>
         /// The user tied to the response of the adaptive card.
         /// </summary>
         [JsonPropertyName("currentUser")]
-        public IAdaptiveCardUser CurrentUser { get; set; }
+        public TUser CurrentUser { get; set; }
 
         /// <summary>
         /// The additional values of the adaptive card.
         /// </summary>
         [JsonPropertyName("value")]
-        public T Value { get; set; }
+        public TValue Value { get; set; }
     }
 }
