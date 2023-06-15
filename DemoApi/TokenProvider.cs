5e9569c794839cc4c2595076ad5d17abbf686110
Derk van Lochem <d.vanlochem@synigo.nl>
2023-06-15 16:12:13 +0200

    First start of a demo solution
    This solution should be easy to build and run, so you can try out all of the possibilities the Api's of Synigo as well as the One API Concepts offer

---
+++ DemoApi/TokenProvider.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+using Synigo.DemoApi.Model;
+
+namespace Synigo.DemoApi
+{
+	public class TokenProvider
+	{
+        private readonly AzureAdSettings _azureAdSettings;
+        private readonly SynigoSettings _synigoSettings;
+
+        public TokenProvider(AzureAdSettings azureSettings, SynigoSettings synigoSettings)
+        {
+            _azureAdSettings = azureSettings;
+            _synigoSettings = synigoSettings;
+        }
+
+        public async Task<string> GetGraphApplicationTokenAsync()
+        {
+            return await GetApplicationToken($"https://graph.microsoft.com/.default");
+        }
+
+        private async Task<string> GetApplicationToken(string scope)
+        {
+            var cca = ConfidentialClientApplicationBuilder
+               .Create(_azureAdSettings.ClientId)
+               .WithClientId(_azureAdSettings.ClientId)
+               .WithClientSecret(_azureAdSettings.ClientSecret)
+               .WithTenantId(_azureAdSettings.TenantId)
+               .WithAuthority(new Uri($"{_azureAdSettings.Instance}/{_azureAdSettings.TenantId}"))
+               .Build();
+
+            var scopes = new string[] { scope };
+            var authResult = await cca.AcquireTokenForClient(scopes)
+                                      .ExecuteAsync();
+            return authResult.AccessToken;
+        }
+
+        public async Task<string> GetSynigoApplicationTokenAsync()
+        {
+            return await GetApplicationToken($"{_synigoSettings.SynigoResourceId}/.default");
+        }
+    }
+}
